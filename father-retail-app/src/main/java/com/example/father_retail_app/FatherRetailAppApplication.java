55f252b811a927bd904c06702d22cec40798fc5d
mustafa <mustafaabdu2004+git@gmail.com>
2026-09-10 10:18:19 +0300

    spring boot setup

---
+++ father-retail-app/src/main/java/com/example/father_retail_app/FatherRetailAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.father_retail_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatherRetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatherRetailAppApplication.class, args);
+	}
+
+}
---
+++ father-retail-app/src/test/java/com/example/father_retail_app/FatherRetailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.father_retail_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatherRetailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
